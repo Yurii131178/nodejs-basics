a9cdabfbdf919d8744e3e7055d730e2a53adab76
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-30 13:16:03 +0300

    add env for MongoDB_add keys to .env.example_create file initMongoDB.js_install mo "mongoose": "^8.16.5"

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import crypto from 'node:crypto';
 
-import { getEnvVar } from './untils/getEnvVar';
+import { getEnvVar } from './utils/getEnvVar.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -19,14 +20,21 @@ export const startServer = () => {
       },
     }),
   );
+  // middleware fo id generation
+  app.use((req, res, next) => {
+    req.id = crypto.randomUUID();
+    next();
+  });
 
+  // controller
   app.get('/', (req, res) => {
     res.json({
       message: 'Hello world!',
+      id: req.id,
     });
   });
 
-  app.use('*', (req, res, next) => {
+  app.use((req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
