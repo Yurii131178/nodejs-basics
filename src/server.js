f77483dbfa8b5941b4c1b83df05f3e740aa78703
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-31 11:59:15 +0300

    place the middlewares in the correct order

--- src/server.js
+++ src/server.js
@@ -68,19 +68,19 @@ export const startServer = () => {
     });
   });
 
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went really wrong',
+      error: err.message,
+    });
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
   app.use((req, res, next) => {
     res.status(404).json({
       message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went really wrong',
-      error: err.message,
     });
   });
 };
